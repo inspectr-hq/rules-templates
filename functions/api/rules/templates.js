79da45de24508177b385be9a29cdac703e0e26d7
Tim Haselaars <timhaselaars@gmail.com>
2025-09-21 12:10:08 +0200

    feat: Allow CORS

--- functions/api/rules/templates.js
+++ functions/api/rules/templates.js
@@ -1,5 +1,15 @@
 import templates from "./templates/data";
 
+const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, OPTIONS",
+    "Access-Control-Allow-Headers": "content-type"
+};
+
 export function onRequestGet() {
-    return Response.json(templates);
+    return Response.json(templates, { headers: corsHeaders });
+}
+
+export function onRequestOptions() {
+    return new Response(null, { status: 204, headers: corsHeaders });
 }
--- functions/api/rules/templates/[id].js
+++ functions/api/rules/templates/[id].js
@@ -1,17 +1,27 @@
 import tpls from "./data";
 
+const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, OPTIONS",
+    "Access-Control-Allow-Headers": "content-type"
+};
+
 export function onRequestGet(context) {
     const id = context.params.id;
 
     if (!id) {
-        return new Response("Not found", { status: 404 });
+        return new Response("Not found", { status: 404, headers: corsHeaders });
     }
 
     const template = tpls.find((template) => template.id === id);
 
     if (!template) {
-        return new Response("Not found", { status: 404 });
+        return new Response("Not found", { status: 404, headers: corsHeaders });
     }
 
-    return Response.json(template);
+    return Response.json(template, { headers: corsHeaders });
+}
+
+export function onRequestOptions() {
+    return new Response(null, { status: 204, headers: corsHeaders });
 }
